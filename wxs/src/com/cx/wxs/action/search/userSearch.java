344a6adbc6684609077ef7836eccd5e865f39c50
zwkkevin92 <2574188587@qq.com>
2016-05-10 15:48:32 +0800

    阿斯蒂芬

---
+++ wxs/src/com/cx/wxs/action/search/userSearch.java
@@ -0,0 +1,11 @@
+package com.cx.wxs.action.search;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cx.wxs.action.BaseAction;
+@Controller
+@RequestMapping
+public class userSearch extends BaseAction {
+
+}
