8d652a3939503315daae3d84570be0892eca2045
zwkkevin92 <2574188587@qq.com>
2016-05-16 22:55:08 +0800

    测试git22
    Signed-off-by: zwkkevin92 <2574188587@qq.com>

--- wxs/src/com/cx/wxs/action/admin/WxsManageAction.java
+++ wxs/src/com/cx/wxs/action/admin/WxsManageAction.java
@@ -38,6 +38,7 @@ public class WxsManageAction {
 	@ResponseBody
 	public String checkAcount(HttpServletRequest request,HttpServletResponse response,WWxsDto wWxsDto){
 		String flag = "0";//默认为0，失败
+		wWxsDto=wxsService.getWWxsByID(wWxsDto);
 		if(wWxsDto!=null&&wWxsDto.getAccount()!=null){
 			List<WWxsDto> list = wxsService.getWWxsList(wWxsDto);
 			if(list!=null&&list.size()>0){
